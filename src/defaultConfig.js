4932ee1bbbe56ba0fb1f2eb70d608bb1c6a697cc
ityike <yuanfeng634@gmail.com>
2018-11-25 22:21:23 +0800

    init project

---
+++ src/defaultConfig.js
@@ -0,0 +1,27 @@
+/**
+ * 项目默认配置项
+ * primaryColor - 默认主题色
+ * navTheme - sidebar theme ['dark', 'light'] 两种主题
+ * colorWeak - 色盲模式
+ * layout - 整体布局方式 ['sidemenu', 'topmenu'] 两种布局
+ * fixedHeader - 固定 Header : boolean
+ *
+ * storageOptions: {} - Vue-ls 插件配置项 (localStorage/sessionStorage)
+ *
+ */
+
+export default {
+  primaryColor: '#1890FF', // primary color of ant design
+  navTheme: 'dark', // theme for nav menu
+  layout: 'sidemenu',
+  fixedHeader: false, // fixed header
+  fixedSideMenu: false,
+  swipeDownHiddenHeader: false,
+  colorWeak: false,
+  // vue-ls options
+  storageOptions: {
+    namespace: 'pro__', // key prefix
+    name: 'ls', // name variable Vue.[ls] or this.[$ls],
+    storage: 'local', // storage name session, local, memory
+  }
+}
